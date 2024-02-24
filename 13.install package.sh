313776fd856135414c9f38f11ea5cc3a0b1c987d
Balumidikondla <balumidikondla153@gmail.com>
2024-02-25 00:28:03 +0530

    somechanges

--- 13.install package.sh
+++ 13.install package.sh
@@ -6,13 +6,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE ="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started exacuting at $TIMESTAMP" &>> $LOGFILE
+
 
 VALIDATE (){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
        echo -e "$2...$R FAILED $N"
     else
