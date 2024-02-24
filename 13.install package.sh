8b9449716f7409ab990eba94fa8210d67a7710ce
Balumidikondla <balumidikondla153@gmail.com>
2024-02-25 00:30:18 +0530

    somechanges

--- 13.install package.sh
+++ 13.install package.sh
@@ -23,7 +23,7 @@ if [ $ID -ne 0 ]
 then 
     echo -e "$R Error ::please run this script with root access $N"
 
-    exit1
+exit1
 else 
     echo  "you are root user"
 fi
@@ -38,3 +38,4 @@ do
        echo -e "$package is already installed ...$Y SKIPPING $N"
 fi
  
+}
