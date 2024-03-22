b12758561149a37ebc41ab612e69f1f159f877bf
Balumidikondla <balumidikondla153@gmail.com>
2024-03-22 15:53:23 +0530

    something

--- 19-delete-old-logs.sh
+++ 19-delete-old-logs.sh
@@ -2,10 +2,10 @@
 
 SOURCE_DIR="/tmp/shellscript-logs"
 
-R="/e[31m
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ ! -d $SOURCE_DIR ]    #DONOTES OPPSITE
 then
@@ -17,7 +17,7 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 #rm -rf esthey saripothundhi but 
 #practice purpose
 
-while IFS= read -r line                    #INTERNAL FIELD SEPARATER
+while IFS= read -r line                                                                          #INTERNAL FIELD SEPARATER
 do
-    echo " Deleting file : $line "  # paina unna line nundi thisukuntundhi
-done <<< FILES_TO_DELETE
+    echo "Deleting file: $line "                                                                # paina unna line nundi thisukuntundhi
+done <<< $FILES_TO_DELETE
