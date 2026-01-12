5e86b895ba85c54780a1dbab3f113b93d3d0ffd2
Balumidikondla <139105863+Balumidikondla@users.noreply.github.com>
2026-01-12 08:51:48 +0530

    Update 04-variables.sh

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$person1: Hello $PERSON1, Good morning"
+echo "$PERSON1: Hello $PERSON1, Good morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
